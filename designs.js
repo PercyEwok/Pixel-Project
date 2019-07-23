abde102ddf82afe9e2e3572603fa725a65762d6d
PercyEwok <53222672+PercyEwok@users.noreply.github.com>
2019-07-23 13:28:13 -0500

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,32 @@
+// Select color input and store
+var color = document.getElementById("colorPicker");
+// Select size input and store
+var sizePicker = document.getElementById("sizePicker");
+// Define variables and store values for DOM elements
+var canvas = document.getElementById("pixelCanvas");
+
+sizePicker.addEventListener("submit", function(event) {
+    event.preventDefault();
+    makeGrid();
+});
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+var height = document.getElementById("inputHeight").value;
+var width = document.getElementById("inputWidth").value;
+canvas.innerHTML = null;
+
+//loop over each row
+for (var m = 0; m < height; m++) {
+    var row = canvas.insertRow(m);
+    for (var n = 0; n < width; n++) {
+        var cell = row.insertCell(n);
+        cell.addEventListener('click', function(event) {
+            // color when a cell is clicked
+            event.target.style.backgroundColor = color.value;
+        });
+      }
+    }
+}
